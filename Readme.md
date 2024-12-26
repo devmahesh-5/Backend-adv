5c5520bf3262f8ba5e592be3d7c22b81543d34a6
Mahesh Bhandari <n31mahesh@gmail.com>
2024-12-26 22:41:55 +0545

    connected mongodb

--- Readme.md
+++ Readme.md
@@ -2,3 +2,15 @@ Advance Backend startup
 
 #  use eraser for data modeling
 - [Eraser](https://app.eraser.io/workspace/bpYGl2nZxnEwVuyo95HV)
+
+# [Go to index](./index.js)
+ 
+ ## how to connect to db
+ - [mongoose documentation](https://mongoosejs.com/docs/index.html)
+ - don't connect db in one line
+ - [we connect mongodb project we created at atlas](https://cloud.mongodb.com/v2/676d76029e97fe11061386a9#/clusters)
+ - atlas database connect aws region behind the scene
+  # Further
+    - npm install mongoose,dotenv,express
+    - there may be problem database connection so always use try catch block
+    - use async await (use IIFE)
---
+++ src/index.js
@@ -0,0 +1,54 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import  connectDB  from "./db/index.js";
+
+
+dotenv.config(
+    {
+        path:"./.env"
+    }
+)
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import express from "express";
+
+const app = express();
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{//database connected but express not listening
+            console.log("Error:",error);
+            throw error;
+        });
+        app.listen(process.env.PORT,()=>{
+            console.log(`App listening at port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("Database connection Error",error);
+        throw error;
+    }
+})() */
