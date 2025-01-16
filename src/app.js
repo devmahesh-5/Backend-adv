7ea539baeea448fb40334639676f6ab863fc42a9
Mahesh Bhandari <n31mahesh@gmail.com>
2025-01-16 16:36:48 +0545

    Like controller Debugged

--- src/app.js
+++ src/app.js
@@ -22,10 +22,12 @@ import tweetRouter from './routes/tweet.routes.js';
 import videoRouter from './routes/video.routes.js';
 import dashboardRouter from './routes/dashboard.routes.js';
 import commentRouter from './routes/comment.routes.js';
+import likeRouter from './routes/like.routes.js';
 //routes declaration
 app.use('/api/v1/users',userRouter);//as user hits /users it passes the control to userRouter
 app.use('/api/v1/tweets',tweetRouter)
 app.use('/api/v1/videos',videoRouter)
 app.use('/api/v1/dashboard',dashboardRouter)
 app.use('/api/v1/comments',commentRouter)
+app.use('/api/v1/likes',likeRouter)
 export {app};
